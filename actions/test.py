4bae96bf85768e4a29d0d8f2c5fe6d706ad049bc
Philipp Scheer <scheer28philipp@gmail.com>
2021-11-11 21:57:16 +0100

    better tests

--- actions/test.py
+++ actions/test.py
@@ -3,9 +3,11 @@ Copyright (c) 2021 Philipp Scheer
 """
 
 
+import time
 from jarvis_sdk import Router
 
 
 @Router.on("status")
 def status():
-    return "up"
+    return { "status": "up", "timestamp": time.time() }
+
--- template.py
+++ template.py
@@ -1,64 +1,20 @@
-import json
-import signal
-import datetime
+import os
 
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jarvis_sdk import PluginServer
+from jarvis_sdk import Logger
 
 from actions import test
 
-from jarvis_sdk import Router
 
-
+CURRENT_WORKING_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 PORT = 6666
 
-logf = open("./template.log", "a+")
-def log(msg):
-	logf.write(f"{msg}\n")
-	logf.flush()
+logger = Logger(f"{CURRENT_WORKING_DIRECTORY}/template.log")
+ps     = PluginServer(PORT, logger.info)
 
+logger.info("Started test plugin")
 
+try:                        ps.handle_requests()
+except KeyboardInterrupt:   pass
 
-
-log("Started test plugin")
-
-running = True
-
-@Router.on("stop")
-def stop_program():
-	global running
-	running = False
-	exit(0)
-
-class PluginHandler(SimpleHTTPRequestHandler):
-	def log_message(self, format, *args):
-		log("%s %s %s" % (datetime.datetime.now(),
-							self.client_address[0],
-							format%args))
-
-	def do_GET(self):
-		self.send_response(405)
-		self.send_header("Content-Type", "application/json")
-		self.end_headers()
-		self.wfile.write(json.dumps({"success": False, "error": "GET parameter not supported. Use POST instead"}).encode("utf-8"))
-		self.wfile.flush()
-
-	def do_POST(self):
-		self.send_response(200)
-		self.send_header("Content-Type", "application/json")
-		self.end_headers()
-
-		res = Router.execute(self.path[1:], None)
-		rsp = { "success": False }
-
-		if res is None:				rsp = { "success": False }
-		elif isinstance(res, bool):	rsp = { "success": res }
-		else:						rsp = { "success": True, "result": res }
-
-		self.wfile.write(json.dumps(rsp).encode("utf-8"))
-		self.wfile.flush()
-
-
-httpd = HTTPServer(("localhost", PORT), PluginHandler)
-while running:
-	httpd.handle_request()
-print("Stopping plugin")
+logger.info("Stopping plugin")
